e9a886b4837f092020609b32031aeaa8ce2e7d27
MiguelonGonzalez <75794654+MiguelonGonzalez@users.noreply.github.com>
2022-02-24 13:44:51 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+  
+  name="dinsar",
+  version="0.1"
+  #license='',
+  author="Miguel González Jiménez",
+  author_email='miguigonn@gmail.com',
+  packages='dinsar',
+  #package_dir={'': 'src'},
+  url='https://github.com/MiguelonGonzalez/dinsar/tutorials',
+  ##keywords='example project',
+  #install_requires=[
+        'numpy',
+        'pandas',
+        'matplotlib',
+        'geopandas',
